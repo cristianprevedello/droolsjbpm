1ede4e2e6cdaeab3e8733a9d15dc2b1635a3776d
Davide Sottara <dsotty@gmail.com>
2011-02-15 01:13:25 +0100

    Added "extends" Test Cases

---
+++ drools-compiler/src/test/java/org/drools/compiler/LegacyBean.java
@@ -0,0 +1,70 @@
+package org.drools.compiler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: davide
+ * Date: 2/12/11
+ * Time: 11:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LegacyBean extends SuperLegacy {
+
+    private String strField;
+    private int intField;
+
+    private Object objField;
+    private Double doubleField;
+
+    public String getStrField() {
+        return strField;
+    }
+
+    public void setStrField(String strField) {
+        this.strField = strField;
+    }
+
+    public int getIntField() {
+        return intField;
+    }
+
+    public void setIntField(int intField) {
+        this.intField = intField;
+    }
+
+    public Object getObjField() {
+        return objField;
+    }
+
+    public void setObjField(Object objField) {
+        this.objField = objField;
+    }
+
+    public Double getDoubleField() {
+        return doubleField;
+    }
+
+    public void setDoubleField(Double doubleField) {
+        this.doubleField = doubleField;
+    }
+
+
+
+}
+
+class SuperLegacy {
+
+        private boolean prop;
+
+        public boolean isProp() {
+            return prop;
+        }
+
+        public void setProp(boolean prop) {
+            this.prop = prop;
+        }
+
+
+        public int myStuff() {
+            return 0;
+        }
+    }
